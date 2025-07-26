03f89398690e30b12837d80a991726d453b3270e
AhmedMaged <111961041+AhmedMagedC@users.noreply.github.com>
2025-07-26 19:54:56 +0300

    Account service (#10)
    * Pushed error messages to Kafka aswell for account & bff services
    
    * Pushed error messages to Kafka aswell for account & bff services

--- account-service/src/main/java/com/example/account_service/handlers/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/example/account_service/handlers/GlobalExceptionHandler.java
@@ -1,23 +1,31 @@
 package com.example.account_service.handlers;
 
 import com.example.account_service.dtos.ErrorDetails;
+import com.example.account_service.enums.MsgType;
 import com.example.account_service.exceptions.BadRequest;
 import com.example.account_service.exceptions.NotFoundException;
+import com.example.account_service.services.AccountService;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @Autowired
+    private AccountService accountService;
+
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorDetails> handleNotFound(NotFoundException ex) {
         ErrorDetails errorDetails = new ErrorDetails(404, "Not Found", ex.getMessage());
+        accountService.sendLog(errorDetails, MsgType.RESPONSE, LocalDateTime.now());
         return ResponseEntity.status(404).body(errorDetails);
     }
 
@@ -39,7 +47,7 @@ public class GlobalExceptionHandler {
                 ErrorDetails errorDetails = new ErrorDetails(400,"Bad Request","Invalid value for enum field '" +
                         fieldName + "'. Allowed values: " +
                         Arrays.toString(targetType.getEnumConstants()));
-
+                accountService.sendLog(errorDetails, MsgType.RESPONSE, LocalDateTime.now());
                 return ResponseEntity.badRequest()
                         .body(errorDetails);
             }
@@ -49,7 +57,7 @@ public class GlobalExceptionHandler {
                 ErrorDetails errorDetails = new ErrorDetails(400,"Bad Request","Invalid numeric format for field '" +
                         fieldName +
                         "'. Expected a valid number.");
-
+                accountService.sendLog(errorDetails, MsgType.RESPONSE, LocalDateTime.now());
                 return ResponseEntity.badRequest()
                         .body(errorDetails);
             }
@@ -58,13 +66,14 @@ public class GlobalExceptionHandler {
             ErrorDetails errorDetails = new ErrorDetails(400,"Bad Request","Invalid value for field '" +
                     fieldName +
                     "': " + ife.getValue());
-
+            accountService.sendLog(errorDetails, MsgType.RESPONSE, LocalDateTime.now());
             return ResponseEntity.badRequest()
                     .body(errorDetails);
         }
 
         // Fallback for malformed JSON or unknown structure
         ErrorDetails errorDetails = new ErrorDetails(400,"Bad Request","Invalid request body: " + ex.getMessage());
+        accountService.sendLog(errorDetails, MsgType.RESPONSE, LocalDateTime.now());
         return ResponseEntity.badRequest()
                 .body(errorDetails);
     }
@@ -72,6 +81,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadRequest.class)
     public ResponseEntity<ErrorDetails> handleBadRequest(BadRequest ex) {
         ErrorDetails errorDetails = new ErrorDetails(400, "Bad Request", ex.getMessage());
+        accountService.sendLog(errorDetails, MsgType.RESPONSE, LocalDateTime.now());
         return ResponseEntity.status(400).body(errorDetails);
     }
 }
